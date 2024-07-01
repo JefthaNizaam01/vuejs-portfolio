fc83d9211f31d89886975d22ecafdbfba6912fcf
Name <nizaam01.jeftha@gmail.com>
2024-07-01 12:11:58 +0200

    storeUpdate

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,128 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+import Swal from 'sweetalert2/dist/sweetalert2'
+
+const portfolioURL = 'https://jefthanizaam01.github.io/EompData/data/'
 
 export default createStore({
   state: {
+    jobTitle: null,
+    about: null,
+    resume: null, 
+    skills: null,
+    testimonials: null,
+    projects: null,
   },
   getters: {
   },
   mutations: {
+    setJobTitle(state, value) {
+      state.jobTitle = value
+    },
+    
+    setAbout(state, value) {
+      state.about = value
+    },
+
+    setResume(state, value) {
+      state.resume = value
+    },
+      setSkills(state, value) {
+        state.skills = value
+      },
+      setTestimonials(state, value) {
+        state.testimonials = value
+      },
+      setProjects(state, value) {
+        state.projects = value
+      }},
+
+  actions: { 
+     async fetchJobTitle(context){
+      try{
+          let {jobTitle}= await (await axios.get(portfolioURL)).data
+      context.commit("setJobTitle", jobTitle)
+    } catch(e){
+      Swal.fire({
+        title: "Error",
+        text: "Failed to fetch the job title",
+        icon: "error",
+        timer: 2000
+      })
+    }
   },
-  actions: {
-  },
+
+  async fetchAbout(context){
+    try{
+        let {about}= await (await axios.get(portfolioURL)).data
+    context.commit("setAbout", about)
+  } catch(e){
+    Swal.fire({
+      title: "Error",
+      text: "Failed to fetch the about",
+      icon: "error",
+      timer: 2000
+    })
+  }
+},
+
+async fetchResume(context){
+  try{
+      let {resume}= await (await axios.get(portfolioURL)).data
+  context.commit("setResume", resume)
+} catch(e){
+  Swal.fire({
+    title: "Error",
+    text: "Failed to fetch the resume",
+    icon: "error",
+    timer: 2000
+  })
+}
+},
+
+async fetchSkills(context){
+  try{
+      let {skills}= await (await axios.get(portfolioURL)).data
+  context.commit("setSkills", skills)
+} catch(e){
+  Swal.fire({
+    title: "Error",
+    text: "Failed to fetch the skills",
+    icon: "error",
+    timer: 2000
+  })
+}
+},
+
+async fetchTestimonials(context){
+  try{
+      let {testimonials}= await (await axios.get(portfolioURL)).data
+  context.commit("setTestimonials", testimonials)
+} catch(e){
+  Swal.fire({
+    title: "Error",
+    text: "Failed to fetch the testimonials",
+    icon: "error",
+    timer: 2000
+  })
+}
+},
+
+async fetchProjects(context){
+  try{
+      let {projects}= await (await axios.get(portfolioURL)).data
+  context.commit("setProjects", projects)
+} catch(e){
+  Swal.fire({
+    title: "Error",
+    text: "Failed to fetch the projects",
+    icon: "error",
+    timer: 2000
+  })
+}
+}
+  
+ },
   modules: {
   }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+  <section class="row" id="home">
+      <LandingSection />
+  </section>
+
+  <section class="row" id="about">
+     <AboutSection />
+  </section>
+  
+  <section class="row" id="resume">
+     <ResumeSection />
+  </section>
+  
+  <section class="row" id="project"></section>
+  
+  <section class="row" id="testimonials"></section>
+  
+  <section class="row" id="reachMe"></section>
+  
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LandingSection from '@/components/LandingSection.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+    name: 'HomeView',
+    components: {
+    
+   
+     
+    }
   }
-}
-</script>
+  </script>
