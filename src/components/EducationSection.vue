0c592cc6e2524e42e20f85806b8a3f3c523aa050
Name <nizaam01.jeftha@gmail.com>
2024-07-01 14:48:50 +0200

    Educationsection

---
+++ src/components/EducationSection.vue
@@ -0,0 +1,58 @@
+<template>
+    <section class="container" id="education">
+      <div class="row">
+        <h2 class="display-2 text-center" id="Ed">Education</h2>
+        <div class="col-xl-12">
+          <ul class="timeline-list">
+            <li>
+              <div class="timeline_content">
+                <span>April 2024 - Present</span>
+                <h4>Student at Life Choices Academy</h4>
+                <p>
+                  I am currently enrolled as a student at Life Choices Academy to become a full stack developer and I am learning a lot. The environment encourages growth and development not only when it comes to learning how to code but also personal development.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="timeline_content">
+                <span>2019-2023</span>
+                <h4>University of the Western Cape</h4>
+                <p>
+                  I obtained my Bachelor of Science degree in Biotechnology and while the journey had its challenges and obstacles, It has taught me a lot about how failure is needed for growth and it takes perseverance and dedication to succeed. It was quite rigorous and at times I definitely had moments of doubt and uncertainty. However, the challenges I faced have shaped me into the resilient and determined individual I am today.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="timeline_content">
+                <span>November 2022 - December 2022</span>
+                <h4>Internship</h4>
+                <p>
+                  I had the opportunity to participate in a science internship that provided me with invaluable experiences and insights into the world of scientific research. Throughout the internship, I was immersed in a dynamic and collaborative environment where I worked alongside seasoned scientists and researchers.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="timeline_content">
+                <span>2013 - 2018</span>
+                <h4>High School</h4>
+                <p>
+                  I went to Grassdale High School and while it was a major adjustment, each obstacle served as an opportunity for growth and self-discovery. High school provided me with a solid foundation upon which to build my future aspirations and pursue my dreams. As I reflect on my high school experience, I am grateful for the memories, lessons, and friendships which I have obtained along the way.
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    name: 'EducationSection',
+  };
+  </script>
+  
+  <style>
+
+  </style>
+  
