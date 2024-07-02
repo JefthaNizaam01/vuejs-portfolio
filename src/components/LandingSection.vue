c2b93765572216fc8a31012f56cec850dda6d522
Name <nizaam01.jeftha@gmail.com>
2024-07-02 08:45:23 +0200

    landingsectionupdate2.0

--- src/components/LandingSection.vue
+++ src/components/LandingSection.vue
@@ -2,7 +2,7 @@
     <main class="container-fluid">
       <section class="container min-vh-100" id="home">
         <div class="row my-4 flex-column flex-md-row align-items-center justify-content-center">
-          <div class="col">
+          <div class="col" data-aos="fade-right">
             <img
               class="img-thumbnail border-5"
               src="https://jefthanizaam01.github.io/favoriteimages/images/Profile2.jpg"
@@ -10,7 +10,7 @@
               loading="lazy"
             />
           </div>
-          <div class="col">
+          <div class="col" data-aos="fade-left">
             <div class="name">
               <h1>Hi, It's <span>Nizaam Jeftha</span></h1>
               <h3 class="developer">I'm an <span>Aspiring Developer</span></h3>
@@ -20,11 +20,16 @@
       </section>
     </main>
   </template>
-  
   <script setup>
-  
+  import { onMounted } from 'vue';
+  import AOS from 'aos';
+  import 'aos/dist/aos.css';
+  onMounted(() => {
+    AOS.init({
+        duration: 1500,
+        once: false
+      });
+    });
   </script>
-  
-  <style>
-</style>
-  
+  <style >
+  </style>
