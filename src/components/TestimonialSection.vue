a76d57ed0eeab8a69d7bb0086cf627489ce45c21
Name <nizaam01.jeftha@gmail.com>
2024-07-02 12:02:47 +0200

    testimonialsectionupdate

---
+++ src/components/TestimonialSection.vue
@@ -0,0 +1,89 @@
+<template>
+    <section class="container" id="testimonials">
+        <div class="row">
+          <h2 class="display-2 text-center justify-content-center" id="testimonial">
+            Testimonials
+          </h2>
+          <div id="carouselExampleFade" class="carousel slide carousel-fade">
+            <div class="carousel-inner">
+              <!-- Aakifah -->
+              <div class="carousel-item text-center active">
+                <img src="https://jefthanizaam01.github.io/favoriteimages/images/kiefah.jpg" class="testimg mx-auto" alt="Testimonial 2">
+                <div class="carousel-content">
+                  <h3>Aakifah Palekar</h3>
+                  <p>"Nizaam is a delightful colleague, exhibiting strong interpersonal skills and a grounded personality. He demonstrates wisdom and possesses exceptional knowledge in his field. Nizaam generously offers assistance and displays remarkable patience. It is indeed an honor to have Nizaam as both a friend and a professional peer."</p>
+                </div>
+              </div>
+              
+              <!-- Shakeel -->
+              <div class="carousel-item text-center">
+                <img src="https://jefthanizaam01.github.io/favoriteimages/images/ShakeelTestimonial.jpg" class="testimg mx-auto" alt="Testimonial 1">
+                <div class="carousel-content">
+                  <h3>Shakeel Davids</h3>
+                  <p>"Nizaam is an excellent coder. He's super dedicated to what he does, and he's really good at it. Whether it's solving tough problems or making cool-looking websites, Nizaam knows his stuff. He's like an artist with code, always coming up with clever solutions. If you need someone to bring your ideas to life online, Nizaam is your guy."</p>
+                </div>
+              </div>
+              
+              <!-- Anushqa -->
+              <div class="carousel-item text-center">
+                <img src="https://jefthanizaam01.github.io/favoriteimages/images/anush.jpg" class="testimg mx-auto" alt="Testimonial 4">
+                <div class="carousel-content">
+                  <h3>Anushqa Kandan</h3>
+                  <p>"Nizaam is an incredibly committed individual who consistently strives for excellence. What sets Nizaam apart is his knack for creating a warm and welcoming environment wherever he goes. He's the kind of person who goes above and beyond, making everyone feel valued and supported. Working with Nizaam is not just about getting the job done, it's about feeling inspired and appreciated every step of the way."</p>
+                </div>
+              </div>
+              
+              <!-- Abdurahmaan -->
+              <div class="carousel-item text-center">
+                <img src="https://jefthanizaam01.github.io/favoriteimages/images/abdu.jpg" class="testimg mx-auto" alt="Testimonial 3">
+                <div class="carousel-content">
+                  <h3>Abdurahmaan Charif</h3>
+                  <p>"Nizaam is a good colleague and a smart worker. He applies himself to all that which he does and has a positive attitude that is contagious who find themselves with him. He will definitely be an asset to any team that he's a part of."</p>
+                </div>
+              </div>
+              
+              <!-- Robyn -->
+              <div class="carousel-item text-center">
+                <img src="https://jefthanizaam01.github.io/favoriteimages/images/RobinTestimonial.jpg" class="testimg mx-auto" alt="Testimonial 5">
+                <div class="carousel-content">
+                  <h3>Robyn Carnow</h3>
+                  <p>"Nizaam is an easy-going and high-spirited person who puts his creative foot first. He approaches challenges with a calm demeanor, never straying from focus when it comes to making a deadline. Charm comes easily to him, coupled with a natural affinity to make one feel at ease, whether that be individually or within a team." </p>
+                </div>
+              </div>
+              
+              <!-- Joel -->
+              <div class="carousel-item text-center">
+                <img id="joel-img" src="https://jefthanizaam01.github.io/favoriteimages/images/joel.png" class="testimg mx-auto" alt="Testimonial 6">
+                <div class="carousel-content">
+                  <h3>Joel Mukanya (Lecturer)</h3>
+                  <p>"Nizaam is a highly creative, well-behaved, and dedicated individual who thrives on collaborating with his peers and excels at working on project assignments. He has acquired various skills and tools, including HTML5, CSS3, Bootstrap, VS Code, and wireframe. I am pleased to introduce him as an exceptional candidate and developer." </p>
+                </div>
+              </div>
+              
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scope>
+#joel-img {
+    width: 300px;
+    height: 400px;
+}
+</style>
+
