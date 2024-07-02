bdcfc59c7d24310ab258cb4f6c51753527f4fc4b
Name <nizaam01.jeftha@gmail.com>
2024-07-02 12:44:13 +0200

    reachmepage

--- src/main.js
+++ src/main.js
@@ -8,5 +8,6 @@ import 'bootstrap'
 import '@/assets/css/style.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 createApp(App).use(store).use(router).mount('#app')
 AOS.init();
