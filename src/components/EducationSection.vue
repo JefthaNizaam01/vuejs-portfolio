cd2a2ef8dec50e3707109899675f3f77e0f38e48
Name <nizaam01.jeftha@gmail.com>
2024-07-05 08:43:23 +0200

    allpageresponsive

--- src/components/EducationSection.vue
+++ src/components/EducationSection.vue
@@ -1,98 +1,119 @@
 <template>
-    <section class="container" id="education">
-      <div class="row">
-        <h2 class="display-2 text-center" id="Ed" data-aos="fade-down">Education</h2>
-        <div class="col-xl-12">
-          <ul class="timeline-list">
-            <li data-aos="fade-left">
-              <div class="timeline_content">
-                <span>April 2024 - Present</span>
-                <h4>Student at Life Choices Academy</h4>
-                <p>
-                  I am currently enrolled as a student at Life Choices Academy to become a full stack developer and I am learning a lot. The environment encourages growth and development not only when it comes to learning how to code but also personal development.
-                </p>
-              </div>
-            </li>
-            <li data-aos="fade-right">
-              <div class="timeline_content">
-                <span>2019-2023</span>
-                <h4>University of the Western Cape</h4>
-                <p>
-                  I obtained my Bachelor of Science degree in Biotechnology and while the journey had its challenges and obstacles, It has taught me a lot about how failure is needed for growth and it takes perseverance and dedication to succeed. It was quite rigorous and at times I definitely had moments of doubt and uncertainty. However, the challenges I faced have shaped me into the resilient and determined individual I am today.
-                </p>
-              </div>
-            </li>
-            <li data-aos="fade-left">
-              <div class="timeline_content">
-                <span>November 2022 - December 2022</span>
-                <h4>Internship</h4>
-                <p>
-                  I had the opportunity to participate in a science internship that provided me with invaluable experiences and insights into the world of scientific research. Throughout the internship, I was immersed in a dynamic and collaborative environment where I worked alongside seasoned scientists and researchers.
-                </p>
-              </div>
-            </li>
-            <li data-aos="fade-right">
-              <div class="timeline_content">
-                <span>2013 - 2018</span>
-                <h4>High School</h4>
-                <p>
-                  I went to Grassdale High School and while it was a major adjustment, each obstacle served as an opportunity for growth and self-discovery. High school provided me with a solid foundation upon which to build my future aspirations and pursue my dreams. As I reflect on my high school experience, I am grateful for the memories, lessons, and friendships which I have obtained along the way.
-                </p>
-              </div>
-            </li>
-          </ul>
-        </div>
+  <section class="container" id="education">
+    <div class="row">
+      <h2 class="display-2 text-center" id="Ed" data-aos="fade-down">Education</h2>
+      <div class="col-xl-12">
+        <ul class="timeline-list">
+          <li data-aos="fade-left">
+            <div class="timeline_content">
+              <span>April 2024 - Present</span>
+              <h4>Student at Life Choices Academy</h4>
+              <p>
+                I am currently enrolled as a student at Life Choices Academy to become a full stack developer and I am learning a lot. The environment encourages growth and development not only when it comes to learning how to code but also personal development.
+              </p>
+            </div>
+          </li>
+          <li data-aos="fade-right">
+            <div class="timeline_content">
+              <span>2019-2023</span>
+              <h4>University of the Western Cape</h4>
+              <p>
+                I obtained my Bachelor of Science degree in Biotechnology and while the journey had its challenges and obstacles, It has taught me a lot about how failure is needed for growth and it takes perseverance and dedication to succeed. It was quite rigorous and at times I definitely had moments of doubt and uncertainty. However, the challenges I faced have shaped me into the resilient and determined individual I am today.
+              </p>
+            </div>
+          </li>
+          <li data-aos="fade-left">
+            <div class="timeline_content">
+              <span>November 2022 - December 2022</span>
+              <h4>Internship</h4>
+              <p>
+                I had the opportunity to participate in a science internship that provided me with invaluable experiences and insights into the world of scientific research. Throughout the internship, I was immersed in a dynamic and collaborative environment where I worked alongside seasoned scientists and researchers.
+              </p>
+            </div>
+          </li>
+          <li data-aos="fade-right">
+            <div class="timeline_content">
+              <span>2013 - 2018</span>
+              <h4>High School</h4>
+              <p>
+                I went to Grassdale High School and while it was a major adjustment, each obstacle served as an opportunity for growth and self-discovery. High school provided me with a solid foundation upon which to build my future aspirations and pursue my dreams. As I reflect on my high school experience, I am grateful for the memories, lessons, and friendships which I have obtained along the way.
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
-    </section>
-  </template>
-  <script>
-  export default {
-    name: 'EducationSection',
-  };
-  </script>
-  <style scoped>
-  @media (max-width: 720px) {
-    #education {
-      padding: 15px;
-      margin: 15px auto;
-      max-width: 90%;
-      text-align: center; 
-    }
-  
-    .timeline_content {
-      font-size: 16px;
-      padding: 15px;
-      text-align: center; 
-    }
-  
-    .timeline_content span,
-    .timeline_content h4,
-    .timeline_content p {
-      text-align: center; 
-    }
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'EducationSection',
+};
+</script>
+<style scoped>
+@media (max-width: 720px) {
+  #education {
+    padding: 15px;
+    margin: 15px auto;
+    max-width: 90%;
+    text-align: center;
   }
-  
-  /* Responsive styles for 300px */
-  @media (max-width: 300px) {
-    #education {
-      padding: 10px;
-      margin: 10px auto;
-      max-width: 100%;
-      text-align: center; 
-    }
-  
-    .timeline_content {
-      font-size: 14px;
-      padding: 5px;
-      text-align: center; 
-      margin: 2px; 
-    }
-  
-    .timeline_content span,
-    .timeline_content h4,
-    .timeline_content p {
-      font-size: 12px;
-      text-align: center;
-    }
+  .timeline_content {
+    font-size: 16px;
+    padding: 15px;
+    text-align: center;
+    margin-bottom: 20px;
   }
-  </style>
+  .timeline_content span,
+  .timeline_content h4,
+  .timeline_content p {
+    text-align: center;
+  }
+  ul.timeline-list:before {
+    display: none;
+  }
+  ul.timeline-list li:before {
+    display: none;
+  }
+  ul.timeline-list li .timeline_content,
+  ul.timeline-list li:nth-child(2n) .timeline_content {
+    width: 80%;
+    text-align: center;
+    float: none;
+    margin: 0 auto 20px;
+  }
+}
+@media (max-width: 300px) {
+  #education {
+    padding: 10px;
+    margin: 10px auto;
+    max-width: 100%;
+    text-align: center;
+  }
+  .timeline_content {
+    font-size: 14px;
+    padding: 5px;
+    text-align: center;
+    margin-bottom: 10px;
+  }
+  .timeline_content span,
+  .timeline_content h4,
+  .timeline_content p {
+    font-size: 12px;
+    text-align: center;
+  }
+  ul.timeline-list:before {
+    display: none;
+  }
+  ul.timeline-list li:before {
+    display: none;
+  }
+  ul.timeline-list li .timeline_content,
+  ul.timeline-list li:nth-child(2n) .timeline_content {
+    width: 90%;
+    text-align: center;
+    float: none;
+    margin: 0 auto 10px;
+  }
+}
+</style>
--- src/components/ReachMeSection.vue
+++ src/components/ReachMeSection.vue
@@ -1,111 +1,121 @@
 <template>
-    <div class="container justify-content-center text-center" id="reachMe">
-      <div class="row">
-        <h2 id="reach" class="display-2 text-center justify-content-center">Reach Me</h2>
-        <section id="formz" class="py-3 py-md-5">
-          <div class="container">
-            <div class="row justify-content-md-center">
-              <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
-                <h2 class="mb-4 display-5 text-center">Let's get in touch</h2>
-                <p class="text-secondary mb-5 text-center">I'm always open to new opportunities and collaborations. Don't hesitate to reach out!</p>
-                <hr class="w-50 mx-auto mb-5 mb-xl-9 border-dark-subtle">
-              </div>
+  <div class="container justify-content-center text-center" id="reachMe">
+    <div class="row">
+      <h2 id="reach" class="display-2 text-center justify-content-center">Reach Me</h2>
+      <section id="formz" class="py-3 py-md-5">
+        <div class="container">
+          <div class="row justify-content-md-center">
+            <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
+              <h2 class="mb-4 display-5 text-center">Let's get in touch</h2>
+              <p class="text-secondary mb-5 text-center">I'm always open to new opportunities and collaborations. Don't hesitate to reach out!</p>
+              <hr class="w-50 mx-auto mb-5 mb-xl-9 border-dark-subtle">
             </div>
           </div>
-          
-          <div class="container text-center">
-            <div class="row justify-content-center">
-              <div class="col">
-                <div class="card-Contact">
-                  <div class="card-body text-black">
-                    <p>
-                      <i class="bi bi-telephone-forward-fill"></i> Phone: 084-734-9535
+        </div>
+        <div class="container text-center">
+          <div class="row justify-content-center">
+            <div class="col">
+              <div class="card-Contact">
+                <div class="card-body text-black">
+                  <p>
+                    <i class="bi bi-telephone-forward-fill"></i> Phone: 084-734-9535
+                  </p>
+                  <p>
+                    <i class="bi bi-envelope"></i> Email: nizaam01.jeftha@gmail.com
+                  </p>
+                  <p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="16" height="16">
+                      <path d="M168 0C75.1 0 0 75.1 0 168c0 87.5 129.7 214.5 152.2 235.2c8.5 7.9 21 7.9 29.5 0C222.3 382.5 352 255.5 352 168C352 75.1 276.9 0 184 0H168zM168 240c-39.8 0-72-32.2-72-72c0-39.8 32.2-72 72-72c39.8 0 72 32.2 72 72C240 207.8 207.8 240 168 240z"/>
+                    </svg> Address: Grassy Park, Cape Town
+                  </p>
+                  <p>
+                      <a id="social-icon" href="https://www.linkedin.com/in/nizaam-jeftha-53684a2a8/" target="_blank" ><i class="bi bi-linkedin"></i></a>
+                      <a id="social-icon" href="https://github.com/JefthaNizaam01" target="_blank" ><i class="bi bi-github"></i></a>
                     </p>
-                    <p>
-                      <i class="bi bi-envelope"></i> Email: nizaam01.jeftha@gmail.com
-                    </p>
-                    <p>
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="16" height="16">
-                        <path d="M168 0C75.1 0 0 75.1 0 168c0 87.5 129.7 214.5 152.2 235.2c8.5 7.9 21 7.9 29.5 0C222.3 382.5 352 255.5 352 168C352 75.1 276.9 0 184 0H168zM168 240c-39.8 0-72-32.2-72-72c0-39.8 32.2-72 72-72c39.8 0 72 32.2 72 72C240 207.8 207.8 240 168 240z"/>
-                      </svg> Address: Grassy Park, Cape Town
-                    </p>
-                    <p>
-                        <a id="social-icon" href="https://www.linkedin.com/in/nizaam-jeftha-53684a2a8/" target="_blank" ><i class="bi bi-linkedin"></i></a>
-                        <a id="social-icon" href="https://github.com/JefthaNizaam01" target="_blank" ><i class="bi bi-github"></i></a>
-                      </p>
-                      
-                  </div>
                 </div>
               </div>
             </div>
           </div>
-  
-          <div class="container text-center justify-content-lg-center">
-            <div class="row justify-content-lg-center">
-              <div class="col-12 col-lg-9">
-                <div>
-                  <form action="https://formspree.io/f/mrgndglz" method="POST" class="formal">
-                    <div class="row gy-4 gy-xl-5 p-4 p-xl-5">
-                      <div class="col-12">
-                        <label for="fullname" class="form-label">Full Name <span class="text-danger">*</span></label>
-                        <input type="text" class="form-control" id="fullname" name="fullname" value="" required>
-                      </div>
-                      <div class="col-12 col-md-6">
-                        <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
-                        <div class="input-group">
-                          <span class="input-group-text">
-                            <i class="bi bi-envelope"></i>
-                          </span>
-                          <input type="email" class="form-control" id="email" name="email" value="" required>
-                        </div>
-                      </div>
-                      <div class="col-12 col-md-6">
-                        <label for="phone" class="form-label">Phone Number</label>
-                        <div class="input-group">
-                          <span class="input-group-text">
-                            <i class="bi bi-telephone"></i>
-                          </span>
-                          <input type="number" class="form-control" id="phone" name="phone" value="">
-                        </div>
-                      </div>
-                      <div class="col-12">
-                        <label for="message" class="form-label">Message <span class="text-danger">*</span></label>
-                        <textarea class="form-control" id="message" name="message" rows="3" required></textarea>
-                      </div>
-                      <div class="col-12">
-                        <div id="resbutton" class="d-grid">
-                          <button id="sub" class="btn btn-primary btn-lg" type="submit">Submit</button>
-                          <button id="sub" class="btn btn-primary btn-lg" type="reset">Clear</button>
-                        </div>
+        </div>
+        <div class="container text-center justify-content-lg-center">
+          <div class="row justify-content-lg-center">
+            <div class="col-12 col-lg-9">
+              <div>
+                <form action="https://formspree.io/f/mrgndglz" method="POST" class="formal">
+                  <div class="row gy-4 gy-xl-5 p-4 p-xl-5">
+                    <div class="col-12">
+                      <label for="fullname" class="form-label">Full Name <span class="text-danger">*</span></label>
+                      <input type="text" class="form-control" id="fullname" name="fullname" value="" required>
+                    </div>
+                    <div class="col-12 col-md-6">
+                      <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
+                      <div class="input-group">
+                        <span class="input-group-text">
+                          <i class="bi bi-envelope"></i>
+                        </span>
+                        <input type="email" class="form-control" id="email" name="email" value="" required>
                       </div>
                     </div>
-                  </form>
-                </div>
+                    <div class="col-12 col-md-6">
+                      <label for="phone" class="form-label">Phone Number</label>
+                      <div class="input-group">
+                        <span class="input-group-text">
+                          <i class="bi bi-telephone"></i>
+                        </span>
+                        <input type="number" class="form-control" id="phone" name="phone" value="">
+                      </div>
+                    </div>
+                    <div class="col-12">
+                      <label for="message" class="form-label">Message <span class="text-danger">*</span></label>
+                      <textarea class="form-control" id="message" name="message" rows="3" required></textarea>
+                    </div>
+                    <div class="col-12">
+                      <div id="resbutton" class="d-grid">
+                        <button id="sub" class="btn btn-primary btn-lg" type="submit">Submit</button>
+                        <button id="sub" class="btn btn-primary btn-lg" type="reset">Clear</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
-    
-        </section>
-      </div>
+        </div>
+      </section>
     </div>
-  </template>
-  
-  <script>
-  export default {
-   
-  }
-  </script>
-  
-  <style scoped>
-  #social-icon {
-    color: black;
-    font-size: 2em; 
-    margin: 0 10px; 
-  }
-  
-  #social-icon:hover {
-    color: black; 
-  }
-  
-  </style>
-  
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+#social-icon {
+  color: black;
+  font-size: 2em;
+  margin: 0 10px;
+}
+#social-icon:hover {
+  color: black;
+}
+@media (min-width: 1080px) {
+#formz {
+  width: 100%;
+  text-align: center;
+}
+}
+@media (max-width: 720px) {
+#resbutton {
+  flex-direction: column;
+  align-items: center;
+  gap:3rem;
+}
+}
+@media (max-width: 300px) {
+#resbutton {
+  flex-direction: column;
+  align-items: center;
+  gap: 3rem;
+}
+}
+</style>
