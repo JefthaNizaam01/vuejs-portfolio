05785d7a6214a5e8987b07f9d437c480722a9336
Nizaam Jeftha <nizaam01.jeftha@gmail.com>
2024-08-01 23:27:18 +0200

    skillsupdate

--- src/components/SkillsSection.vue
+++ src/components/SkillsSection.vue
@@ -1,87 +1,158 @@
 <template>
-    <section class="container" id="skills">
-        <div class="row">
-            <h2 class="display-2 text-center" id="skill">Skills</h2>
-            <div class="row row-cols-1 row-cols-md-3 g-4">
-                <div class="col" data-aos="fade-up">
-                  <div class="card h-100">
-                    <div class="card-body" id="skillglow">
-                      <h5 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-filetype-html" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M14 4.5V3a1 1 0 0 0-1-1H9.707l-.853-.854A1 1 0 0 0 8.5 1H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-.5H4.8v1H2v-10h6.5v1h4.2V5h.5v-.5h-.5V4.5zM2 12V2h6.5v2.5H2V12zm5-5.5V8H5v1.5h2v1.5H4.5v2h5V5.5H9v1.5H7V5.5h-.5z"/></svg> HTML5</h5>
-                      <p class="card-text">Proficient - in HTML5 with extensive experience in crafting modern and responsive web layouts.</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col" data-aos="fade-up">
-                  <div class="card h-100">
-                    <div class="card-body" id="skillglow">
-                      <h5 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-filetype-css" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.415 3.42l-.72 8.333L8 13.972l6.306-2.218-.72-8.334L8 2.03 2.415 3.42zm-.824 1.085 1.268-.393.468 5.426 3.208.883v1.893L7.02 12.07 3.385 10.7l.254-2.95 2.359-.7-.282-3.26L1.59 4.505zm11.942 0l-4.298-1.268.282 3.26-2.36.7-.253 2.95L6.98 12.07l1.245.424v-1.893l3.208-.883.468-5.426 1.268.393z"/></svg> CSS</h5>
-                      <p class="card-text">Intermediate - in CSS for styling web elements with moderate complexity and responsiveness.</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col" data-aos="fade-up">
-                  <div class="card h-100">
-                    <div class="card-body" id="skillglow">
-                      <h5 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-filetype-js" viewBox="0 0 16 16"><path d="M1.64 3.5v9.986h5.474V3.5H1.64zm.97 8.995h3.535v-8.01H2.61v8.01zm6.477-8.995v9.986h5.474V3.5H9.087zm.97 8.995h3.535v-8.01H10.06v8.01z"/></svg> JavaScript</h5>
-                      <p class="card-text">Beginner level - Exploring the basics of JavaScript, developing simple scripts for basic functionality.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="row row-cols-1 row-cols-md-3 g-4">
-                <div class="col" data-aos="fade-up">
-                  <div class="card h-100">
-                    <div class="card-body" id="skillglow">
-                      <h5 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-puzzle" viewBox="0 0 16 16"><path d="M8.499.5A1.5 1.5 0 0 0 7 2v2.5h2V2a1.5 1.5 0 0 0-1-1.415zm3.5 0A1.5 1.5 0 0 0 10 2v2.5h2V2a1.5 1.5 0 0 0-1-1.415zm3.5 0A1.5 1.5 0 0 0 13 2v2.5h2V2a1.5 1.5 0 0 0-1-1.415zM8 4.5H6V8h2V4.5zm4 0h-2V8h2V4.5zm4 0h-2V8h2V4.5zM8 9.5H6v4h2v-4zm4 0h-2v4h2v-4zm4 0h-2v4h2v-4zM7 10a1.5 1.5 0 1 0 0 3h2a1.5 1.5 0 1 0 0-3H7zm6 0a1.5 1.5 0 1 0 0 3h2a1.5 1.5 0 1 0 0-3h-2zm6 0a1.5 1.5 0 1 0 0 3h2a1.5 1.5 0 1 0 0-3h-2z"/></svg> Problem Solving</h5>
-                      <p class="card-text">Adept at solving problems by breaking them down into manageable steps and finding creative solutions.</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col" data-aos="fade-up">
-                  <div class="card h-100">
-                    <div class="card-body" id="skillglow">
-                      <h5 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chat-square-text" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v11.293l-1.5-1.5H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v11.586a1 1 0 0 0 .293.707l2.829 2.828a1 1 0 0 0 .707.293H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M3 3h10v1H3V3zm0 2h10v1H3V5zm0 2h10v1H3V7z"/></svg> Communication Skills</h5>
-                      <p class="card-text">Good at communicating ideas clearly and working well with others to get things done.</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col" data-aos="fade-up">
-                  <div class="card h-100">
-                    <div class="card-body" id="skillglow">
-                      <h5 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16"><path d="M8 3.5a.5.5 0 0 1 .5.5v4.25l2.625 1.553a.5.5 0 1 1-.5.866L8 9V4a.5.5 0 0 1 .5-.5z"/><path d="M8 1a7 7 0 1 1 0 14A7 7 0 0 1 8 1zM2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8z"/></svg> Time Management</h5>
-                      <p class="card-text">Expert at managing time wisely, ensuring each moment counts toward achieving objectives.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+  <section class="container" id="skills">
+    <div class="row">
+      <h2 class="display-2 text-center" id="skill" data-aos="fade-up">Skills</h2>
+      <section class="skills-tools" id="skills-tools">
+        <div class="wrapper">
+          <div class="container-icons" data-aos="fade-up">
+            <div class="box-icon">
+              <i class="devicon devicon-html5-plain" aria-hidden="true"></i>
+              <p class="skillheading">HTML5</p>
+            </div>
+            <div class="box-icon">
+              <i class="devicon devicon-css3-plain" aria-hidden="true"></i>
+              <p class="skillheading">CSS3</p>
+            </div>
+            <div class="box-icon">
+              <i class="fab fa-js" aria-hidden="true"></i>
+              <p class="skillheading">JavaScript</p>
+            </div>
+            <div class="box-icon">
+              <i class="devicon devicon-jquery-plain" aria-hidden="true"></i>
+              <p class="skillheading">jQuery</p>
+            </div>
+            <div class="box-icon">
+              <i class="devicon devicon-react-original" aria-hidden="true"></i>
+              <p class="skillheading">React</p>
+            </div>
+            <div class="box-icon">
+              <i class="devicon devicon-git-plain" aria-hidden="true"></i>
+              <p class="skillheading">Git</p>
+            </div>
+            <div class="box-icon">
+              <i class="devicon devicon-github-plain" aria-hidden="true"></i>
+              <p class="skillheading">GitHub</p>
+            </div>
+            <div class="box-icon">
+              <i class="fas fa-cogs" aria-hidden="true"></i>
+              <p class="skillheading">RESTful APIs</p>
+            </div>
+            <div class="box-icon">
+              <i class="fas fa-database" aria-hidden="true"></i>
+              <p class="skillheading">Firebase</p>
+            </div>
+            <div class="box-icon">
+              <i class="devicon devicon-sass-original" aria-hidden="true"></i>
+              <p class="skillheading">SASS</p>
+            </div>
+            <div class="box-icon">
+              <i class="fas fa-terminal" aria-hidden="true"></i>
+              <p class="skillheading">Command Line</p>
+            </div>
+            <div class="box-icon">
+              <i class="devicon devicon-visualstudio-plain" aria-hidden="true"></i>
+              <p class="skillheading">VS Code</p>
+            </div>
+            <div class="box-icon">
+              <i class="fas fa-mobile-alt" aria-hidden="true"></i>
+              <p class="skillheading">Responsive Websites</p>
+            </div>
+            <div class="box-icon">
+              <i class="fas fa-universal-access" aria-hidden="true"></i>
+              <p class="skillheading">Web Accessibility</p>
+            </div>
+            <div class="box-icon">
+              <i class="devicon devicon-wordpress-plain" aria-hidden="true"></i>
+              <p class="skillheading">WordPress</p>
+            </div>
+            <div class="box-icon">
+              <i class="fas fa-file-alt" aria-hidden="true"></i>
+              <p class="skillheading">Adobe Suite</p>
+            </div>
+          </div>
         </div>
-    </section>
+      </section>
+    </div>
+  </section>
 </template>
 
 <script>
-    export default {
-       
-    }
+export default {
+  name: 'SkillsSection'
+ 
+}
 </script>
 
 <style scoped>
-@media only screen and (max-width: 720px) {
-  .row-cols-md-3 .col {
-    flex: 0 0 100%;
-    max-width: 100%;
-    display: flex;
-    justify-content: center;
-  }
-  .card {
-    max-width: 100%;
-    margin: 0 auto;
+
+.skillheading {
+  color: red;
+  font-family: 'Helvetica', Helvetica; 
+  font-size: 1.2rem; 
+  margin-top: 0.5rem; 
+  margin-bottom: 0; 
+  text-shadow: 0 0 1px red,
+  0 0 1px red, 
+  0 0 1px red;
+
+  
+}
+
+
+#skill {
+  margin-bottom: 3rem; 
+}
+
+/* Container for icons */
+.container-icons {
+  text-align: center;
+  margin-bottom: 3rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center; 
+}
+
+
+.container-icons .box-icon {
+  width: 50%;
+  height: 142px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center; 
+  justify-content: center; 
+ 
+}
+
+/* Set all icons to white */
+.container-icons .box-icon i {
+  color: #fff;
+  font-size: 4.5rem;
+  text-shadow: 0 0 1px white,
+  0 0 5px white;
+  
+
+}
+
+/* Hover effect for icon boxes */
+.container-icons .box-icon:hover {
+  box-shadow: 0 0 5px red,
+   0 0 10px red,
+   0 0 40px red; 
+  transform: scale(1.05); 
+}
+
+@media (min-width: 600px) {
+  .container-icons .box-icon {
+    width: 25%;
   }
 }
 
-@media screen and (max-width: 300px) {
-  .row-cols-md-3 {
-    justify-content: center;
-    margin-left: 35px;
+@media (min-width: 1024px) {
+  .container-icons .box-icon {
+    width: 12.5%;
   }
 }
+
+
 </style>
